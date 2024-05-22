67550db6221d9c0b9e4854617d7cc16925706bf1
Calum Marshall CS2019 <calum.marshall.2019@uni.strath.ac.uk>
2024-05-22 11:43:14 +0100

    3.2 Adding type signatures

--- Hello.hs
+++ Hello.hs
@@ -2,16 +2,32 @@ main =
     putStrLn myhtml
 
 myhtml = 
-    makeHtml "My page title" "My page content"
+    makeHtml "Haskell Blog Generator" (h1_ "Welcome to Haskell" <> p_ "This is my blog")
 
-html_ content = 
-    "<html>" <> content <> "</html>"
-body_ content = 
-    "<body>" <> content <> "</body>"
-head_ content = 
-    "<head>" <> content <> "</head>"
-title_ content = 
-    "<title>" <> content <> "</title>"
+html_ :: String -> String
+html_ = el "html"
 
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+  
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_  = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+
+makeHtml :: String -> String -> String
 makeHtml str1 str2 = 
     html_ (head_ (title_ str1) <> body_ str2)
