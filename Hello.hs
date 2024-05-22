f774cc91d58917527bf3685fe9aeb9d9501a3569
Calum Marshall CS2019 <calum.marshall.2019@uni.strath.ac.uk>
2024-05-22 10:52:21 +0100

    3.1. Flexible HTML content

--- Hello.hs
+++ Hello.hs
@@ -1,3 +1,17 @@
-main = putStrLn myhtml
+main = 
+    putStrLn myhtml
 
-myhtml = "<html><body>Hello, world!</body></html>"
+myhtml = 
+    makeHtml "My page title" "My page content"
+
+html_ content = 
+    "<html>" <> content <> "</html>"
+body_ content = 
+    "<body>" <> content <> "</body>"
+head_ content = 
+    "<head>" <> content <> "</head>"
+title_ content = 
+    "<title>" <> content <> "</title>"
+
+makeHtml str1 str2 = 
+    html_ (head_ (title_ str1) <> body_ str2)
