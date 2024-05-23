dc56099fe820c2db8c2c0674f02d2c5cf2929235
Calum Marshall CS2019 <calum.marshall.2019@uni.strath.ac.uk>
2024-05-23 14:01:23 +0100

    3.4 Safter HTML construction with types

--- Hello.hs
+++ Hello.hs
@@ -1,33 +1,45 @@
-main = 
-    putStrLn myhtml
+main :: IO ()
+main = putStrLn (render myhtml)
 
+myhtml :: Html
 myhtml = 
-    makeHtml "Haskell Blog Generator" (h1_ "Welcome to Haskell" <> p_ "This is my blog")
+    html_ "Haskell Blog Generator" (append_ (h1_ "Welcome to Haskell")  (append_ (p_ "This is my blog") (p_ "And this is another paragraph")))
 
-html_ :: String -> String
-html_ = el "html"
+newtype Html = Html String
 
-body_ :: String -> String
-body_ = el "body"
+newtype Structure = Structure String
 
-head_ :: String -> String
-head_ = el "head"
-  
-title_ :: String -> String
-title_ = el "title"
+type Title = String
 
-p_ :: String -> String
-p_  = el "p"
 
-h1_ :: String -> String
-h1_ = el "h1"
+html_ :: Title -> Structure -> Html
+html_ title body = 
+    Html (el "html" (getStructureString body))
 
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 
-makeHtml :: String -> String -> String
-makeHtml str1 str2 = 
-    html_ (head_ (title_ str1) <> body_ str2)
+append_ :: Structure -> Structure -> Structure 
+append_ (Structure s) (Structure s2) = Structure (s <> s2)
+
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
+
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+
+
